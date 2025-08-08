3fa51a28cc64922798705d434e8bfc8bf8794031
dimgeo <dg@biodys.com>
2025-08-08 15:34:16 +0200

    commit

---
+++ confi.py
@@ -0,0 +1,10 @@
+# Configuration file for SBD server
+# Keep this file out of version control!
+
+SIGNAL_CLI_USER = "+3163209409"      # <-- Your signal phone number
+ROCKBLOCK_URL = "https://rockblock.rock7.com/rockblock/MT"
+ROCKBLOCK_USERNAME = "sdf@sdffsdf.com"  # Replace with actual username
+ROCKBLOCK_PASSWORD = "myassword"  # Replace with actual password
+ROCKBLOCK_IMEI = 3009093244700
+POLL_INTERVAL = 30  # seconds
+ALLOWED_NUMBERS_FILE = "signal.allowed"  # Whitelist numbers
